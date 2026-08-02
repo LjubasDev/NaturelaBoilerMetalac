006f2e3d1f89c6fde39bf15b7e4000ec623f8a96
LjubasDev <78151587+LjubasDev@users.noreply.github.com>
2026-08-02 18:36:24 +0200

    Update const.py

--- custom_components/naturela_smartboiler/const.py
+++ custom_components/naturela_smartboiler/const.py
@@ -1,19 +1,34 @@
 DOMAIN = "naturela_smartboiler"
 
+
 BASE_URL = "https://iot.naturela-bg.com"
 
-LOGIN_URL = f"{BASE_URL}/Account/Login"
+
+LOGIN_URL = (
+    BASE_URL +
+    "/Account/Login"
+)
+
 
 STATUS_URL = (
-    f"{BASE_URL}/api/smartboiler/{{device_id}}"
+    BASE_URL +
+    "/api/smartboiler/{device_id}"
 )
 
-SET_STATE_URL = (
-    f"{BASE_URL}/api/smartboiler/setState"
-)
 
 SET_TEMPERATURE_URL = (
-    f"{BASE_URL}/api/smartboiler/setTemperature"
+    BASE_URL +
+    "/api/smartboiler/setTemperature"
 )
 
-DEFAULT_SCAN_INTERVAL = 30
+
+SET_STATE_URL = (
+    BASE_URL +
+    "/api/smartboiler/setState"
+)
+
+
+SET_HEATER_URL = (
+    BASE_URL +
+    "/api/smartboiler/setHeater"
+)
